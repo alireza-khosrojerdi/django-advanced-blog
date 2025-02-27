276ebbc148adb371a637411e25a92d6f05afaaa0
alireza <alirezakhosrojerdi1999@gmail.com>
2025-02-28 00:13:13 +0330

    Todo app
    Todo app with CBV

---
+++ core/todo/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path, include
+from .views import (TaskList ,TaskCreate ,TaskComplete ,TaskUpdate ,DeleteView)
+
+
+
+urlpatterns = [
+    path('', TaskList.as_view(), name='task_list'),
+    path('create/', TaskCreate.as_view(), name='create_task'),
+    path('update/<int:pk>/', TaskUpdate.as_view(), name='update_task'),
+    path('complete/<int:pk>/', TaskComplete.as_view(), name='complete_task'),
+    path('delete/<int:pk>/', DeleteView.as_view(), name='delete_task'),
+]
